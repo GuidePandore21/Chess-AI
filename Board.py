2e5d9ee17f3905d39f7ef519bf79a36718973b2c
Thomas Lyautey <tlyautey1@gmail.com>
2025-08-18 16:04:01 +0200

    Feat : Création de la méthode coords_to_index de la classe Board.py

--- Board.py
+++ Board.py
@@ -4,3 +4,14 @@ class Board:
         self.FILES = "abcdefgh"
         self.RANKS = "12345678"
     
+    def coords_to_index(self, file_index, rank_index):
+        """Converts file and rank indices to a board index.
+        Args:
+            file_index (int): Index of the file (0-7).
+            rank_index (int): Index of the rank (0-7).
+        Returns:
+            int: The board index corresponding to the file and rank indices.
+        Raises:
+            ValueError: If file_index or rank_index are out of bounds (0-7).
+        """
+        return rank_index << 4 | file_index
